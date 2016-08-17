bddb1bb43c415da5c69d877cb2d10667383ef43f
Mark Baggett <mbagget1@utk.edu>
2016-08-16 22:19:08 -0400

    Add code to find Pids in a collection.

--- delete_datastreams.py
+++ delete_datastreams.py
@@ -1,21 +1,36 @@
 import yaml
-import requests
+import urllib
+import xmltodict
+import json
 
 credentials = yaml.load(open('credentials.yaml', 'r'))
-fedora_url = credentials['fedoraurl']
-user_name = credentials['username']
-password = credentials['password']
 collection = "pid%7E{0}%2A".format(credentials['collection_namespace'])
 data_stream = credentials['datastream']
-find_objects_string = fedora_url + ":8080/fedora/objects?query=" + collection + "&pid=true&resultFormat=xml"
+find_objects_string = credentials['fedoraurl'] + ":8080/fedora/objects?query=" + collection \
+                      + "&pid=true&resultFormat=xml&maxResults=2"
+collection_objects = []
+initial_token = ''
 
-
-def get_collection_objects():
-    return
+def get_collection_objects(url, token, pids):
+    x = urllib.request.urlopen(url + token)
+    s = x.read()
+    json_string = json.dumps(xmltodict.parse(s))
+    data = json.loads(json_string)
+    new_token = ''
+    if 'listSession' in data['result']:
+        new_token = data['result']['listSession']['token']
+    object_lists = data['result']['resultList']['objectFields']
+    for item in object_lists:
+        pids.append(item['pid'])
+    if new_token:
+        token = '&sessionToken={0}'.format(new_token)
+        get_collection_objects(url, token, pids)
+    return pids
 
 
 def purge_a_dsid():
     return
 
 if __name__ == "__main__":
-    get_collection_objects(find_objects_string)
+    pids_in_collection = get_collection_objects(find_objects_string, initial_token, collection_objects)
+    print(pids_in_collection)
